ff9f9e653c57dae60dce1b3c77bc55771632252a
yuanyuan.wang <yuanyuan.wang@tinno.com>
2018-05-18 13:56:32 +0800

    <2018.5.17><枚举型单例>

--- app/src/main/java/com/fantasy/android/demo/java/Singleton.java
+++ app/src/main/java/com/fantasy/android/demo/java/Singleton.java
@@ -6,9 +6,21 @@ package com.fantasy.android.demo.java;
 
 public class Singleton {
 
-    private static Singleton mSingleton = new Singleton();
+/*    private static Singleton mSingleton = new Singleton();
 
     public static Singleton getInstance() {
         return mSingleton;
+    }*/
+
+    enum MySingleton {
+        INSTATNCE;
+
+        private void testMethod() {
+            System.out.print("testMethod");
+        }
+    }
+
+    public static void main(String args[]) {
+        MySingleton.INSTATNCE.testMethod();
     }
 }
--- app/src/main/java/com/fantasy/android/demo/java/Singleton3.java
+++ app/src/main/java/com/fantasy/android/demo/java/Singleton3.java
@@ -10,7 +10,7 @@ public class Singleton3 {
 
 /*    private static volatile Singleton3 mInstance;
 
-    public Singleton3 getInstance() {
+    public Singleton3 fgetInstance() {
         if (mInstance == null) {
             synchronized (Singleton3.this) {
                 mInstance = new Singleton3();
@@ -24,10 +24,10 @@ public class Singleton3 {
     }
 
     public static class SingletonFactory {
-        public static final Singleton3 mInstance = new Singleton3();
+        private static final Singleton3 mInstance = new Singleton3();
     }
 
-    public Singleton3 getInstance() {
+    public static Singleton3 getInstance() {
         return SingletonFactory.mInstance;
     }
 }
